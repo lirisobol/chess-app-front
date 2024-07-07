301d09a7a518f8f4551fe32d9ed6c5cdf709fea3
Liri <lirisobol16@gmail.com>
2024-07-07 21:28:01 +0300

    added icons for w/l/d & removed <best> section & fixed more details button + icon

--- src/components/CommonModeCard/CommonModeCard.tsx
+++ src/components/CommonModeCard/CommonModeCard.tsx
@@ -1,6 +1,8 @@
 import { ChessVariantStats } from "../../models/UserStats";
 import localStyles from "./CommonModeCard.module.css";
-import externalLinkIcon from "../../assets/images/external_link_icon_dark.svg";
+import { faArrowUpRightFromSquare ,faTrophy, faTimesCircle, faHandshake} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 interface CommonModeCardProps {
     mode: string;
@@ -11,14 +13,11 @@ function CommonModeCard({mode, stats, icon}: CommonModeCardProps):JSX.Element {
     if (!stats) {
         return <div className={localStyles.commonModeCard}>No data available for {mode}</div>;
     }
-    const handleViewGameClick = () => {
-        window.open(stats.best.game, '_blank', 'noopener,noreferrer');
-    };
+
     const handleMoreDetailsLink = () => {
         // TEMPORARY FOR TESTING !
         window.open(stats.best.game, '_blank', 'noopener,noreferrer');
     };
-
 
     return (
         <div className={localStyles.commonModeCard}>
@@ -33,53 +32,39 @@ function CommonModeCard({mode, stats, icon}: CommonModeCardProps):JSX.Element {
             <div className="vr"></div>
 
             <div className={localStyles.cardRecordSection}>
-                <div className={localStyles.variantRecord}>
+                <div className={localStyles.variantRecordContainer}>
                     <div>
-                        <p className={localStyles.recordTitle}>Wins</p>
+                        <FontAwesomeIcon icon={faTrophy}/>
+                        <p className={localStyles.recordTitle}> Wins</p>
                         <p className={localStyles.recordScore}>{stats.record.win}</p>
                     </div>
                     <div>
-                        <p className={localStyles.recordTitle}>Losses</p>
+                        <FontAwesomeIcon icon={faTimesCircle}/>
+                        <p className={localStyles.recordTitle}> Losses</p>
                         <p className={localStyles.recordScore}>{stats.record.loss}</p>
                     </div>
                     <div>
-                        <p className={localStyles.recordTitle}>Draws</p>
+                        <FontAwesomeIcon icon={faHandshake}/>
+                        <p className={localStyles.recordTitle}> Draws</p>
                         <p className={localStyles.recordScore}>{stats.record.draw}</p>
                     </div>
                 </div>
             </div>
             <div className="vr"></div>
 
-
             <div className={localStyles.cardBestSection}>
-                <h5>Best Game</h5>
-                <div className={localStyles.variantBest}>
-                    <div>
-                        <p className={localStyles.bestTitle}>Date</p>
-                        <p className={localStyles.bestScore}>{new Date(stats.best?.date * 1000).toLocaleDateString()}</p>
-                    </div>
-                    <div>
-                        <p className={localStyles.bestTitle}>Rating</p>
-                        <p className={localStyles.bestScore}>{stats.best?.rating}</p>
-                    </div>
-                </div>
-                <button 
-                    className="btn btn-dark rounded-pill" 
-                    onClick={handleViewGameClick}
-                    >
-                        View Game <img src={externalLinkIcon} alt="External Link Icon"/>
-                    </button>
-
+                future rating trend or more stats
             </div>
 
             <div className="vr"></div>
 
             <div className={localStyles.moreDetailsLink}>
                 <button 
-                    className="btn btn-dark rounded-pill" 
+                    className="btn btn-outline-dark rounded-pill" 
                     onClick={handleMoreDetailsLink}
                     >
-                    More Details <img src={externalLinkIcon} alt="External Link Icon"/>
+                    More Details 
+                    <FontAwesomeIcon icon={faArrowUpRightFromSquare} className={localStyles.buttonIcon} />
                 </button>
             </div>
 
